bc84cc0927561618d76563fab8923672580d5b83
ngocdang2005 <phimma2002a@gmail.com>
2025-08-25 02:28:08 +0700

    category

--- src/pages/categories/addCategory.tsx
+++ src/pages/categories/addCategory.tsx
@@ -1,9 +1,74 @@
-import React from 'react'
+import api from "@/config/axios.customize";
+import { ICategory } from "@/types/categories";
+import { Button, Form, Input, message, Select } from "antd";
+import { useNavigate } from "react-router";
 
-const addCategory = () => {
+const AddCategory = () => {
+  const { TextArea } = Input;
+  const nav = useNavigate();
+  const [form] = Form.useForm();
+  const onFinish = async (values: ICategory) => {
+    try {
+      await api.post("api/categories/", values);
+      message.success("Thêm danh mục thành công!");
+      nav("/categories");
+    } catch (err: any) {
+      if (err.response?.status === 400) {
+        message.error(err.response.data.message || "Tên danh mục đã tồn tại");
+      } else {
+        message.error("Có lỗi xảy ra, vui lòng thử lại!");
+      }
+    }
+  };
+
   return (
-    <div>addCategory</div>
-  )
-}
+    <>
+      <Form form={form} layout="vertical" onFinish={onFinish} style={{ maxWidth: 600, margin: "0 auto" }}>
+        <Form.Item
+          label="Tên"
+          name="name"
+          rules={[
+            { required: true, message: "Vui lòng nhập tên danh mục" },
+            { min: 3, message: "Ít nhất 3 ký tự" },
+          ]}
+        >
+          <Input placeholder="VD: Tiểu thuyết" />
+        </Form.Item>
+        <Form.Item
+          label="Tên đường dẫn"
+          name="slug"
+          rules={[
+            { required: true, message: "Vui lòng nhập tên đường dẫn" },
+            { min: 3, message: "Ít nhất 3 ký tự" },
+          ]}
+        >
+          <Input placeholder="VD: tieu-thuyet" />
+        </Form.Item>
+        <Form.Item label="Trạng thái" name="status" initialValue="active">
+          <Select>
+            <Select.Option value="active">Mở</Select.Option>
+            <Select.Option value="inactive">Khoá</Select.Option>
+          </Select>
+        </Form.Item>
+        <Form.Item
+          label="Mô tả"
+          name="description"
+          rules={[
+            { required: true, message: "Vui lòng nhập mô tả" },
+            { min: 10, message: "Mô tả ít nhất 10 ký tự" },
+          ]}
+        >
+          <TextArea rows={3} placeholder="Mô tả hiển thị" />
+        </Form.Item>
 
-export default addCategory
+        <Form.Item>
+          <Button type="primary" htmlType="submit" block>
+            Xác nhận
+          </Button>
+        </Form.Item>
+      </Form>
+    </>
+  );
+};
+
+export default AddCategory;
--- src/routers/routers.tsx
+++ src/routers/routers.tsx
@@ -1,6 +1,9 @@
 import { Route, Routes } from "react-router";
 import PrivateRouters from "./private.routers";
 import Analytics from "@/pages/analytics/analytics";
+import Category from "@/pages/categories/categories";
+import AddCategory from "@/pages/categories/addCategories";
+import EditCategory from "@/pages/categories/editCategories";
 
 const Routers = () => {
   const isAuthenticated = true;
@@ -12,6 +15,10 @@ const Routers = () => {
           <Route index element={<Analytics />} />
           <Route path="/analytics" element={<Analytics />} />
         </Route>
+
+        <Route path="/categories" element={<Category />} />
+        <Route path="/categories/add" element={<AddCategory />} />
+        <Route path="/categories/update/:id" element={<EditCategory />} />
       </Route>
     </Routes>
   );
